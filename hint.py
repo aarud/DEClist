38d9acab4e6ddc4b4f57c2b4bd2c5a9b65aa70ec
aarud <ada6363t@gmail.com>
2015-09-27 23:09:40 -0500

    Update hint.py

--- hint.py
+++ hint.py
@@ -2,13 +2,12 @@
 # Python uses spacing instead of brackets {} and ; to tell where statements end.
 # So, the whole body of a for-loop has to be tabbed once more than the "for" statement. The for-loop ends when a line is tabbed less than the "for" statement.
 # Also, arrays are called "lists" in Python.
-# Here is how the logic of your 7-line code will look:
+# Here is how the logic of your code will look:
 
 #binarydump = "[paste the giant binary string here]"
 #import sys (this allows printing characters without newlines between them)
 
 #declare a for loop to read from the beginning (0) to the end (hint: use "len()") that "steps" 6 characters ahead between each read (because each word is 6 bits long)
-	#hexc = "{0:0>2}".format([hex value of the 6 binary characters you read][2:])
-	#find the position (index) of each hex value you read in hexlist
-	#find the character in asciilist corresponding to that position
+	#find the value of each 6-bit binary character. The value = the position of a character in asciilist.
+	#Find the character in asciilist corresponding to that value/position
 	#sys.stdout.write(the character)
